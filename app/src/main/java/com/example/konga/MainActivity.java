36677b04e3e18e4f451507b20526865ac1888646
hex636b <chen.d.kang@gmail.com>
2018-12-27 13:11:23 +0800

    request RECORD_AUDIO permission at runtime

--- app/src/main/java/com/example/konga/MainActivity.java
+++ app/src/main/java/com/example/konga/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.konga;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
     private static final String TAG = "MainActivity";
+    private static final int REQUEST_RECORD_AUDIO = 0x1;
+
     private Button mButtonStart;
     private Button mButtonStop;
     private AudioIntercom audioIntercom;
@@ -23,6 +28,15 @@ public class MainActivity extends Activity {
 
             @Override
             public void onClick(View v) {
+
+                if(checkSelfPermission(android.Manifest.permission.RECORD_AUDIO)
+                        != PackageManager.PERMISSION_GRANTED){
+                    requestPermissions(
+                            new String[]{android.Manifest.permission.RECORD_AUDIO},
+                            REQUEST_RECORD_AUDIO);
+                    return;
+                }
+
                 if(audioIntercom != null ){
                     audioIntercom.destroy();
                 }
@@ -44,5 +58,16 @@ public class MainActivity extends Activity {
         });
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if(requestCode == REQUEST_RECORD_AUDIO && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            mButtonStart.callOnClick();
+        }else {
+            Toast.makeText(this,"user denied RECORD_AUDIO", Toast.LENGTH_SHORT).show();
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
 
 }
